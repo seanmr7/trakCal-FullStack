43186a664a0eddc7d364ca67ba5381cbd6d1e127
Sean <smriedl@gmail.com>
2022-03-01 19:21:41 -0500

    start work on frontend with create-react-app

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ connectDB()
 const app = express()
 
 app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: false }))
 
 // Routes
 app.use('/api/users', require('./routes/userRoutes'))

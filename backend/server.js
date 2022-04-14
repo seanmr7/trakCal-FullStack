986e4b6c736ab93b0c853c4ad6726a18018e599b
Sean <smriedl@gmail.com>
2022-04-14 10:41:08 -0400

    Prepare for heroku deploy

--- backend/server.js
+++ backend/server.js
@@ -29,7 +29,7 @@ if (process.env.NODE_ENV === 'production') {
   })
 } else {
   app.get('/', (req, res) => {
-    res.status(200).json({ message: 'Welcome to the Support Desk API' })
+    res.status(200).json({ message: 'Welcome to TrakCal' })
   })
 }
 
